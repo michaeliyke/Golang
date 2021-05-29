a6caae62e29fb3f2558a0ae7ea927f5bae93d42c
michaeliyke <ymichaelc@gmail.com>
2021-05-29 16:16:12 +0100

    go.mod - for dependency mgt
    Use a dependency to write attractive Hello messages

--- test.go
+++ test.go
@@ -1,10 +1,22 @@
-// You can edit this code
-// Click here and start typing
+/*
+Glass() - Glass returns a useful phrase for world travelers.
+Go() - Go returns a Go proverb.
+Hello() - Hello returns a greeting.
+Opt() - Opt returns an optimization truth.
+*/
 
 package main
 
 import "fmt"
+import "rsc.io/quote"
 
 func main() {
-	fmt.Println("Hello world!")
+	const x = 40
+	var y = 10
+	// var random = rand(12000)
+	fmt.Println("Hello world!", x, y)
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
 }
