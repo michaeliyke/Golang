b99854b906e3dd82f68b7f07e888367608ee0151
michaeliyke <ymichaelc@gmail.com>
2021-05-31 21:56:02 +0100

    Short variable declarations

--- Basics/package.go
+++ Basics/package.go
@@ -5,14 +5,14 @@ import (
 )
 
 /*
-A var declaration can include initializers, one per variable.
-
-If an initializer is present, the type can be omitted;
-the variable will take the type of the initializer.
+Inside a function, the := short assignment statement can be used in place of a var
+declaration with implicit type.
+Outside a function, every statement begins with a keyword (var, func, and so on) and so
+ the := construct is not available
 */
-var i, j int = 1, 2
 
 func main() {
-	var c, python, java = true, false, "no"
+	var i, j int = 1, 2
+	c, python, java := true, false, "no"
 	fmt.Println(i, j, c, python, java)
 }
