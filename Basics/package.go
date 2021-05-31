c80f2b7da6043954ee5f7e95bd54eb113ada5736
michaeliyke <ymichaelc@gmail.com>
2021-05-31 21:09:20 +0100

    Exported names: Names starting with capital letter are expoted while names starting with lower case are not

--- Basics/package.go
+++ Basics/package.go
@@ -1,15 +1,16 @@
 // Programs start running in package main.
 package main
 
-// By convention, the package name is the same as the last element of the import path
-// the "math/rand" package comprises files that begin with the statement package rand
+// In Go, a name is exported if it begins with a capital letter.
+// For example, Pizza is an exported name, as is Pi, which is exported from the math package
+
+// When importing a package, you can refer only to its exported names.
+// Any "unexported" names are not accessible from outside the package.
 import (
 	"fmt"
 	"math"
-	"math/rand"
 )
 
 func main() {
-	fmt.Printf("Now you have %g problems \n", math.Sqrt(7))
-	fmt.Println("My favorite number is ", rand.Intn(10))
+	fmt.Println("Value of pi is: ", math.Pi)
 }
