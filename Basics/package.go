2459b836e0e35a172684f31a141afa40813c091d
michaeliyke <ymichaelc@gmail.com>
2021-05-31 22:25:05 +0100

    Constants

--- Basics/package.go
+++ Basics/package.go
@@ -3,22 +3,19 @@ package main
 import "fmt"
 
 /*
-When declaring a variable without specifying an explicit type (either by using the :=
-	syntax or var = expression syntax), the variable's type is inferred from the value on
-	 the right hand side.
+Constants are declared like variables, but with the const keyword.
 
-When the right hand side of the declaration is typed, the new variable is of that same type:
+Constants can be character, string, boolean, or numeric values.
 
-var i int
-j := i // j is an int
-But when the right hand side contains an untyped numeric constant, the new variable may
- be an int, float64, or complex128 depending on the precision of the constant:
-
-i := 42           // int
-f := 3.142        // float64
-g := 0.867 + 0.5i // complex128
+Constants cannot be declared using the := syntax.
 */
+const Pi = 3.14
+
 func main() {
-	v := 4.00212 // change me!
-	fmt.Printf("v is of type %T\n", v)
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
 }
