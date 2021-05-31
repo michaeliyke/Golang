fbd91ad41972d25683026eacfb17c1849accf136
michaeliyke <ymichaelc@gmail.com>
2021-05-31 22:28:38 +0100

    Numeric Constants

--- Basics/package.go
+++ Basics/package.go
@@ -2,20 +2,21 @@ package main
 
 import "fmt"
 
-/*
-Constants are declared like variables, but with the const keyword.
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
 
-Constants can be character, string, boolean, or numeric values.
-
-Constants cannot be declared using the := syntax.
-*/
-const Pi = 3.14
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
 
 func main() {
-	const World = "世界"
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go rules?", Truth)
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
