d57f56e3ad055e74ca3e430aeb2a8461efa5d2e5
michaeliyke <ymichaelc@gmail.com>
2021-05-31 21:48:51 +0100

    Variables

--- Basics/package.go
+++ Basics/package.go
@@ -4,16 +4,9 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println(split(17))
-}
+var c, python, java bool
 
-// Go's return values may be named, eg x int, y int.
-// If so, they are treated as variables defined at the top of the function
-// A return statement without arguments returns the named return values.
-// This is known as a "naked" return.
-func split(sum int) (x int, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
 }
