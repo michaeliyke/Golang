a177a93b92fafb349f0f1f6a747e8d9995e3da27
michaeliyke <ymichaelc@gmail.com>
2021-05-31 22:02:32 +0100

    Basic types

--- Basics/package.go
+++ Basics/package.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 )
 
 /*
-Inside a function, the := short assignment statement can be used in place of a var
-declaration with implicit type.
-Outside a function, every statement begins with a keyword (var, func, and so on) and so
- the := construct is not available
+bool
+
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // alias for uint8
+
+rune // alias for int32
+     // represents a Unicode code point
+
+float32 float64
+
+complex64 complex128
+The example shows variables of several types, and also that variable declarations may be
+ "factored" into blocks, as with import statements.
+
+The int, uint, and uintptr types are usually 32 bits wide on 32-bit systems and 64 bits
+ wide on 64-bit systems. When you need an integer value you should use int unless you
+ have a specific reason to use a sized or unsigned integer type.
 */
 
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
 func main() {
-	var i, j int = 1, 2
-	c, python, java := true, false, "no"
-	fmt.Println(i, j, c, python, java)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
