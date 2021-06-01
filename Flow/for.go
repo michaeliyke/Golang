61f1c172824c6467e3ad6d8c712368d40247ef4c
michaeliyke <ymichaelc@gmail.com>
2021-06-01 22:18:19 +0100

    Switch evaluation order

--- Flow/for.go
+++ Flow/for.go
@@ -1,33 +1,22 @@
 package main
 
 import (
-	"runtime"
+	"time"
 
 	"example.com/log"
 )
 
-/*
-A switch statement is a shorter way to write a sequence of
-if - else statements. It runs the first case whose value is equal
- to the condition expression.
-
-Go's switch is like the one in C, C++, Java, JavaScript, and PHP,
- except that Go only runs the selected case, not all the cases
-  that follow. In effect, the break statement that is needed
-  at the end of each case in those languages is provided
-  automatically in Go. Another important difference is that Go's
-   switch cases need not be constants, and the values involved
-    need not be integers.
-*/
-
 func main() {
-	log.Print("Go runs on: ")
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		log.Print("OS X")
-	case "linux":
-		log.Print("Linux")
+	log.P("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		log.P("Today.")
+	case today + 1:
+		log.P("Tomorrow.")
+	case today + 2:
+		log.P("In two days.")
 	default:
-		log.Print(os)
+		log.P("Too far away.")
 	}
 }
