cdc4c96b96b5698cace6f7ed53517b3a2cb6d12a
michaeliyke <ymichaelc@gmail.com>
2021-06-01 21:52:24 +0100

    Exercise: Loops and Functions

--- Flow/for.go
+++ Flow/for.go
@@ -1,26 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "example.com/log"
 
-/*
-Variables declared inside an if short statement are also
- available inside any of the else blocks.
-*/
-func main() {
-	fmt.Println(
-		pow(3, 2, 10),
-		pow(3, 3, 20),
-	)
+func Sqrt(x float64) float64 {
+	var j float64
+	for z := float64(1); z*z < x+1; z++ {
+		if z*z < x {
+			j = z
+		}
+		if z*z == x {
+			return z
+		}
+		if z*z > x {
+			return j
+		}
+	}
+	return j
 }
 
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
-	}
-	return lim
+func main() {
+	log.Log(Sqrt(99))
 }
