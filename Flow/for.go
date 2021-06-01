b434b859e6e6350582a6ecb4f131c98da86cfc53
michaeliyke <ymichaelc@gmail.com>
2021-06-01 22:14:16 +0100

    Switch
    add more methods to the log package

--- Flow/for.go
+++ Flow/for.go
@@ -1,23 +1,33 @@
 package main
 
-import "example.com/log"
+import (
+	"runtime"
 
-func Sqrt(x float64) float64 {
-	var j float64
-	for z := float64(1); z*z < x+1; z++ {
-		if z*z < x {
-			j = z
-		}
-		if z*z == x {
-			return z
-		}
-		if z*z > x {
-			return j
-		}
-	}
-	return j
-}
+	"example.com/log"
+)
+
+/*
+A switch statement is a shorter way to write a sequence of
+if - else statements. It runs the first case whose value is equal
+ to the condition expression.
+
+Go's switch is like the one in C, C++, Java, JavaScript, and PHP,
+ except that Go only runs the selected case, not all the cases
+  that follow. In effect, the break statement that is needed
+  at the end of each case in those languages is provided
+  automatically in Go. Another important difference is that Go's
+   switch cases need not be constants, and the values involved
+    need not be integers.
+*/
 
 func main() {
-	log.Log(Sqrt(99))
+	log.Print("Go runs on: ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		log.Print("OS X")
+	case "linux":
+		log.Print("Linux")
+	default:
+		log.Print(os)
+	}
 }
--- log/log.go
+++ log/log.go
@@ -5,3 +5,23 @@ import "fmt"
 func Log(x interface{}) {
 	fmt.Println(x)
 }
+
+func L(x interface{}) {
+	fmt.Println(x)
+}
+
+func Pf(x interface{}) {
+	fmt.Print(x)
+}
+
+func Print(x interface{}) {
+	fmt.Print(x)
+}
+
+func P(x interface{}) {
+	fmt.Println(x)
+}
+
+func Sp(x string) string {
+	return fmt.Sprintf(string(x))
+}
