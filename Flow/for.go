3082936e69599bf3c260b32f9df940169d618a17
michaeliyke <ymichaelc@gmail.com>
2021-06-01 17:55:27 +0100

    If and else

--- Flow/for.go
+++ Flow/for.go
@@ -6,23 +6,21 @@ import (
 )
 
 /*
-Like for, the if statement can start with a short statement to
-execute before the condition.
-
-Variables declared by the statement are only in scope until the
-end of the if.
+Variables declared inside an if short statement are also
+ available inside any of the else blocks.
 */
-
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	}
-	return lim
-}
-
 func main() {
 	fmt.Println(
 		pow(3, 2, 10),
 		pow(3, 3, 20),
 	)
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
 }
