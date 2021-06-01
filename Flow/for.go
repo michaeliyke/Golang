6500fd94e11fc48529e921975ae8e1f1dca17697
michaeliyke <ymichaelc@gmail.com>
2021-06-01 17:48:56 +0100

    If with a short statement

--- Flow/for.go
+++ Flow/for.go
@@ -6,18 +6,23 @@ import (
 )
 
 /*
-Go's if statements are like its for loops;
-the expression need not be surrounded by parentheses ( )
-but the braces { } are required.
+Like for, the if statement can start with a short statement to
+execute before the condition.
+
+Variables declared by the statement are only in scope until the
+end of the if.
 */
 
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
 	}
-	return fmt.Sprint(math.Sqrt(x))
+	return lim
 }
 
 func main() {
-	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }
