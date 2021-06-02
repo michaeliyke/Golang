bb00e5dbd0fa337d92d1d860042430844c37b9b8
michaeliyke <ymichaelc@gmail.com>
2021-06-02 16:21:35 +0100

    Nil slices

---
+++ Types/nil-slices.go
@@ -0,0 +1,27 @@
+package main
+
+import "example.com/log"
+
+/*
+The zero value of a slice is nil.
+A nil slice has a length and capacity of 0 and has no underlying
+ array.
+*/
+
+func main() {
+	var s []int
+	P(s)
+
+	if s == nil {
+		log.P("Nil")
+	}
+}
+
+func P(x []int) {
+	log.Print(x)
+	log.Print(":- ")
+	log.Print("len - ")
+	log.Print(len(x))
+	log.Print(" cap - ")
+	log.Println(cap(x))
+}
