3deb6526a5cb3f99fa811cb91ae362bc469ad82d
michaeliyke <ymichaelc@gmail.com>
2021-06-02 17:40:25 +0100

    Range

---
+++ Types/range.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/log"
+)
+
+/*
+The range form of the for loop iterates over a slice or map.
+
+When ranging over a slice, two values are returned for each
+iteration. The first is the index, and the second is a copy of
+the element at that index.
+*/
+
+func main() {
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+func P(x []int, s string) {
+	log.Print(s + ":- ")
+	log.Print("len - ")
+	log.Print(len(x))
+	log.Print(" cap - ")
+	log.Print(cap(x))
+	log.Print(" value - ")
+	log.Println(x)
+}
