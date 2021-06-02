8df3b40d60e64c5fa8ccf74a37f49473c097e976
michaeliyke <ymichaelc@gmail.com>
2021-06-02 23:28:07 +0100

    Re-write example.com/log to correctly implement methods
    Make methods accept infinite parameters

--- log/log.go
+++ log/log.go
@@ -2,48 +2,78 @@ package log
 
 import "fmt"
 
-func typeof(x interface{}) {
-	var typeof string = fmt.Sprintf("%T", x)
-	// fmt.Printf("%T\n", x)
-	P(typeof)
+func typeof(x interface{}, types ...string) bool {
+	if len(types) > 0 {
+		var answer bool = false
+		var typeofx string = fmt.Sprintf("%T", x)
+		for _, t := range types {
+			if t == typeofx {
+				answer = true
+				break
+			}
+		}
+		return answer
+	}
+	fmt.Printf("%T\n", x)
+	return false
 }
 
-func ShowType(x interface{}) {
-	typeof(x)
+func ShowType(x interface{}, types ...string) bool {
+	return typeof(x, types...)
 }
 
-func Type(x interface{}) {
-	typeof(x)
+func TypeOf(x interface{}, types ...string) bool {
+	return typeof(x, types...)
 }
 
-func TypeOf(x interface{}) {
-	typeof(x)
+func Type(x interface{}, types ...string) bool {
+	return typeof(x, types...)
 }
 
-func Log(x interface{}) {
-	fmt.Println(x)
+func sprintf(str string, args ...interface{}) string {
+	return fmt.Sprintf(str, args...)
 }
 
-func L(x interface{}) {
-	fmt.Println(x)
+func print(x ...interface{}) {
+	fmt.Print(x...)
 }
 
-func Pf(x interface{}) {
-	fmt.Print(x)
+func printf(x string, args ...interface{}) {
+	fmt.Printf(x, args...)
 }
 
-func Print(x interface{}) {
-	fmt.Print(x)
+func println(x ...interface{}) {
+	fmt.Println(x...)
 }
 
-func Println(x interface{}) {
-	fmt.Println(x)
+func Log(x ...interface{}) {
+	Println(x...)
 }
 
-func P(x interface{}) {
-	fmt.Println(x)
+func L(x ...interface{}) {
+	Println(x...)
 }
 
-func Sp(x string) string {
-	return fmt.Sprintf(string(x))
+func Pf(str string, args ...interface{}) {
+	printf(str, args...)
+}
+
+func Print(x ...interface{}) {
+	print(x...)
+}
+
+func Println(x ...interface{}) {
+	println(x...)
+}
+
+func P(x ...interface{}) {
+	Println(x...)
+}
+
+func Sp(str string, args ...interface{}) string {
+	return sprintf(str, args...)
+}
+
+func Sprintf(str string, args ...interface{}) string {
+	return sprintf(str, args...)
 }
