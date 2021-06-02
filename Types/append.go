33a67bb6a9efe924b7a2af909aadd4e33f1503f4
michaeliyke <ymichaelc@gmail.com>
2021-06-02 17:39:59 +0100

    Appending to a slice

---
+++ Types/append.go
@@ -0,0 +1,28 @@
+package main
+
+import "example.com/log"
+
+func main() {
+	var s []int
+	P(s, "[]int")
+
+	// Append works on nil slices
+	s = append(s, 0)
+	P(s, "append(s, 0)")
+
+	s = append(s, 1)
+	P(s, "append(s, 1)")
+
+	s = append(s, 2, 3, 4)
+	P(s, "append(s, 2, 3, 4)")
+}
+
+func P(x []int, s string) {
+	log.Print(s + ":- ")
+	log.Print("len - ")
+	log.Print(len(x))
+	log.Print(" cap - ")
+	log.Print(cap(x))
+	log.Print(" value - ")
+	log.Println(x)
+}
