85a44e6aa7972a17fb5ec171c63a456a02314c7c
michaeliyke <ymichaelc@gmail.com>
2021-06-03 09:14:10 +0100

    Function closures - create add function

---
+++ Types/function-closures.go
@@ -0,0 +1,28 @@
+package main
+
+import "example.com/log"
+
+/*
+Function closures
+Go functions may be closures. A closure is a function value that
+ references variables from outside its body. The function may
+ access and assign to the referenced variables; in this sense the
+ function is "bound" to the variables.
+For example, the adder function returns a closure. Each closure
+is bound to its own sum variable.
+*/
+
+func adder() func(int) int {
+	var sum int = 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	var positive, negative func(int) int = adder(), adder()
+	for index := 0; index < 10; index++ {
+		log.Println(positive(index), negative(-2*index))
+	}
+}
