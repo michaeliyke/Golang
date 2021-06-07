7d517f8784464748a45ccca6207fcdc0ba13ad89
michaeliyke <ymichaelc@gmail.com>
2021-06-07 10:11:40 +0100

    Methods are functions

---
+++ Methods/methods-func.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math"
+
+	"example.com/log"
+)
+
+/*
+Remember: a method is just a function with a receiver argument.
+
+Here's Abs written as a regular function with no change in functionality.
+*/
+
+type Vertex struct {
+	X, Y float64
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	log.Println(Abs(v))
+}
