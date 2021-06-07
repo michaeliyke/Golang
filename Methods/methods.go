cddabde53d78f696aba8b450af231666a2ddd886
michaeliyke <ymichaelc@gmail.com>
2021-06-07 10:01:09 +0100

    Methods

---
+++ Methods/methods.go
@@ -0,0 +1,31 @@
+/*
+Go does not have classes. However, you can define methods on types.
+
+A method is a function with a special receiver argument.
+
+The receiver appears in its own argument list between the func keyword and the method
+ name.
+
+In this example, the Abs method has a receiver of type Vertex named v.
+*/
+
+package main
+
+import (
+	"math"
+
+	"example.com/log"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func main() {
+	v := Vertex{3, 4}
+	log.Println(v.Abs())
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
