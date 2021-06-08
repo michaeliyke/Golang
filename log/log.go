838f67501a1e7e6e2f412fc3e3e203738cce50cb
michaeliyke <ymichaelc@gmail.com>
2021-06-08 11:07:19 +0100

    Add Printf method
    COrrect few bad naming

--- log/log.go
+++ log/log.go
@@ -2,78 +2,82 @@ package log
 
 import "fmt"
 
-func typeof(x interface{}, types ...string) bool {
+func typeof(datum interface{}, types ...string) bool {
 	if len(types) > 0 {
 		var answer bool = false
-		var typeofx string = fmt.Sprintf("%T", x)
+		var typeofdatum string = fmt.Sprintf("%T", datum)
 		for _, t := range types {
-			if t == typeofx {
+			if t == typeofdatum {
 				answer = true
 				break
 			}
 		}
 		return answer
 	}
-	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", datum)
 	return false
 }
 
-func ShowType(x interface{}, types ...string) bool {
-	return typeof(x, types...)
+func ShowType(datum interface{}, types ...string) bool {
+	return typeof(datum, types...)
 }
 
-func TypeOf(x interface{}, types ...string) bool {
-	return typeof(x, types...)
+func TypeOf(datum interface{}, types ...string) bool {
+	return typeof(datum, types...)
 }
 
-func Type(x interface{}, types ...string) bool {
-	return typeof(x, types...)
+func Type(datum interface{}, types ...string) bool {
+	return typeof(datum, types...)
 }
 
 func sprintf(str string, args ...interface{}) string {
 	return fmt.Sprintf(str, args...)
 }
 
-func print(x ...interface{}) {
-	fmt.Print(x...)
+func print(args ...interface{}) {
+	fmt.Print(args...)
 }
 
-func printf(x string, args ...interface{}) {
-	fmt.Printf(x, args...)
+func printf(str string, args ...interface{}) {
+	fmt.Printf(str, args...)
 }
 
-func println(x ...interface{}) {
-	fmt.Println(x...)
+func println(args ...interface{}) {
+	fmt.Println(args...)
 }
 
-func Log(x ...interface{}) {
-	Println(x...)
-}
-
-func L(x ...interface{}) {
-	Println(x...)
+func Printf(str string, args ...interface{}) {
+	printf(str, args...)
 }
 
 func Pf(str string, args ...interface{}) {
 	printf(str, args...)
 }
 
-func Print(x ...interface{}) {
-	print(x...)
+func Log(args ...interface{}) {
+	Println(args...)
 }
 
-func Println(x ...interface{}) {
-	println(x...)
+func L(args ...interface{}) {
+	Println(args...)
 }
 
-func P(x ...interface{}) {
-	Println(x...)
+func Print(args ...interface{}) {
+	print(args...)
 }
 
-func Sp(str string, args ...interface{}) string {
-	return sprintf(str, args...)
+func Println(args ...interface{}) {
+	println(args...)
+}
+
+func P(args ...interface{}) {
+	Println(args...)
 }
 
 func Sprintf(str string, args ...interface{}) string {
+	return sprintf(str, args...)
+}
+
+func Sp(str string, args ...interface{}) string {
 	return sprintf(str, args...)
 }
