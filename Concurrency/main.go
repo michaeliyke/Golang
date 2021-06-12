68a7b2a36dca1e0eec1cb453772e6e85f699c846
michaeliyke <ymichaelc@gmail.com>
2021-06-12 19:46:26 +0100

    Concurrency: stage 1
    Use time.Sleep() to trigger delay in-between executions

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -12,19 +12,21 @@ var random = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	for i := 0; i < 10; i++ {
 		id := rand.Intn(10) + 1
-		if b, ok := queryCache(id); ok {
-			log.Println("From cache")
-			log.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			log.Println("From Database")
-			log.Println(b)
-			continue
-		}
-		log.Printf("Book not found with id: %v", id)
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				log.Println("From cache")
+				log.Println(b)
+			}
+		}(id)
+		go func(id int) {
+			if b, ok := queryDatabase(id); ok {
+				log.Println("From Database")
+				log.Println(b)
+			}
+		}(id)
 		time.Sleep(150 * time.Millisecond)
 	}
+	time.Sleep(2 * time.Second)
 }
 
 func queryDatabase(id int) (Book, bool) {
