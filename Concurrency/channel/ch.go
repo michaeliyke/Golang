5bbd8d2ad8332558fd258097faaa17fdbdc04273
michaeliyke <ymichaelc@gmail.com>
2021-06-15 17:04:56 +0100

    Send only and Receive only channel example
    Buffered channel used here not not necesary

--- Concurrency/channel/ch.go
+++ Concurrency/channel/ch.go
@@ -7,14 +7,15 @@ import (
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	ch := make(chan int, 1) // Buffered channel takes size arg to indicate how many msgs can wait
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		log.L(<-ch)
 		wg.Done()
 	}(ch, wg)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
+		ch <- 27 // This will wait to no avail for receiver
 		wg.Done()
 	}(ch, wg)
 	wg.Wait()
