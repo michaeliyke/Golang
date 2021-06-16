8dcf242b333bc5f590dc2f4b1023f211d02605aa
michaeliyke <ymichaelc@gmail.com>
2021-06-16 12:25:38 +0100

    Working with JSON

---
+++ WS/json/main2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+
+	"example.com/log"
+)
+
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
+}
+
+func main() {
+	product := &Product{
+		ProductID:      123,
+		Manufacturer:   "Big Box Comapany",
+		PricePerUnit:   "12.99",
+		Sku:            "4561qHJK",
+		Upc:            "414654444566",
+		QuantityOnHand: 28,
+		ProductName:    "Gizmo",
+	}
+
+	productJSON, err := json.Marshal(product)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(productJSON))
+}
