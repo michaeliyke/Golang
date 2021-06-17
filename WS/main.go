1ecb1b6ca99bf3beb0ad5c3d9d5af8b138d86b9d
michaeliyke <ymichaelc@gmail.com>
2021-06-17 10:31:31 +0100

    Retrieve and update single product item

--- WS/main.go
+++ WS/main.go
@@ -1,49 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func productHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		productsJSON, error := json.Marshal(productList)
-		if error != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "aplication/json")
-		w.Write(productsJSON)
-	case http.MethodPost:
-		// Create a bew product
-		var newProduct Product
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = json.Unmarshal(bodyBytes, &newProduct)
-		if err != nil {
-			w.WriteHeader(http.StatusBadGateway)
-			return
-		}
-		if newProduct.ProductID != 0 {
-			w.WriteHeader(http.StatusBadGateway)
-			return
-		}
-		newProduct.ProductID = getNextID()
-		productList = append(productList, newProduct)
-		w.WriteHeader(http.StatusCreated)
-		log.Println(newProduct)
-		return
-	}
-}
-
 func main() {
 	log.Println("Hello")
-	http.HandleFunc("/products", productHandler)
+	http.HandleFunc("/products", productsHandler)
+	http.HandleFunc("/products/", productHandler)
 	http.ListenAndServe(":80", nil)
 }
